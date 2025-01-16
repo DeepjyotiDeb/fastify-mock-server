1e9502af9673a36f82e23d338ba63b72e32e6a5c
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2025-01-16 15:18:59 +0530

    updated delays

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-unused-vars */
-import crypto from "crypto";
+import crypto from 'crypto';
 
-import fastifyWebsocket from "@fastify/websocket";
-import Fastify from "fastify";
-import cors from "@fastify/cors";
-import fs from "fs/promises";
-import fsWrite from "fs";
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
-import { speakText } from "./utils/speechToText.js";
-import { randomPhrase } from "./utils/randomPhrase.js";
+import fastifyWebsocket from '@fastify/websocket';
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
+import fs from 'fs/promises';
+import fsWrite from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { speakText } from './utils/speechToText.js';
+import { randomPhrase } from './utils/randomPhrase.js';
 
 const fastify = Fastify({
   bodyLimit: 100 * 1024 * 1024,
@@ -18,7 +18,8 @@ const multipartUploads = new Map();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const UPLOAD_DIR = path.join(__dirname, "uploads");
+const UPLOAD_DIR = path.join(__dirname, 'uploads');
+
 (async () => {
   try {
     await fs.access(UPLOAD_DIR);
@@ -36,13 +37,13 @@ fastify.register(cors, {
   // 'x-CSRFToken',
   // ],
   credentials: true,
-  exposedHeaders: ["etag"],
-  origin: ["http://localhost:5173"],
+  exposedHeaders: ['etag'],
+  origin: ['http://localhost:5173'],
   // methods: ['GET', 'POST', 'PUT', 'DELETE'],
 });
 fastify.addContentTypeParser(
-  ["application/octet-stream", "application/pdf"],
-  { parseAs: "buffer" },
+  ['application/octet-stream', 'application/pdf'],
+  { parseAs: 'buffer' },
   function (req, body, done) {
     done(null, body);
   }
@@ -50,55 +51,55 @@ fastify.addContentTypeParser(
 
 fastify.register(fastifyWebsocket);
 
-fastify.get("/api/get-csrf-token/", (_req, res) => {
-  return { csrfToken: crypto.randomBytes(8).toString("hex") };
+fastify.get('/api/get-csrf-token/', (_req, res) => {
+  return { csrfToken: crypto.randomBytes(8).toString('hex') };
 });
 
 fastify.register(async function (fastify) {
   fastify.get(
-    "/ws/voice/:interviewId/:candidateId/",
+    '/ws/voice/:interviewId/:candidateId/',
     { websocket: true },
     (ws /* WebSocket */, req /* FastifyRequest */, params) => {
-      console.log("url");
+      console.log('url');
 
       const interviewId = req.params?.interviewId;
       const candidateId = req.params?.candidateId;
 
-      ws.on("message", async (text) => {
+      ws.on('message', async (text) => {
         // message.toString() === 'hi from client'
-        console.log("passing message to client", text.toString());
+        console.log('passing message to client', text.toString());
 
         const sampleText = randomPhrase();
         const base64Audio = await speakText(sampleText);
         ws.send(
           JSON.stringify({
-            type: "ai_response",
+            type: 'ai_response',
             audio_url: base64Audio,
             text: sampleText,
           })
         );
       });
-      ws.on("close", () => {
-        console.log("Client disconnected");
+      ws.on('close', () => {
+        console.log('Client disconnected');
       });
 
-      ws.on("error", (error) => {
-        console.error("WebSocket error:", error);
+      ws.on('error', (error) => {
+        console.error('WebSocket error:', error);
       });
     }
   );
 });
 
-fastify.get("/api", async (req, reply) => {
-  return { message: "Hello, Fastify!" };
+fastify.get('/api', async (req, reply) => {
+  return { message: 'Hello, Fastify!' };
 });
 
-fastify.post("/api/init-multipart-upload/", async (req, res) => {
+fastify.post('/api/init-multipart-upload/', async (req, res) => {
   try {
-    console.log("init multipart upload");
+    console.log('init multipart upload');
     const { fileName, fileType, interviewId, candidate_id } = req.body;
     // Generate unique upload ID (simulating S3's upload ID)
-    console.log("candidate_id:", candidate_id);
+    // console.log('candidate_id:', candidate_id);
     const uploadId = crypto.randomUUID().slice(0, 8);
     const key = `${interviewId}/${candidate_id}/${uploadId}`;
 
@@ -107,26 +108,26 @@ fastify.post("/api/init-multipart-upload/", async (req, res) => {
       filename: fileName,
       parts: new Map(),
       startTime: Date.now(),
-      status: "in-progress",
+      status: 'in-progress',
       key,
     });
     await new Promise((resolve) => setTimeout(resolve, 500));
-    return { uploadId, key, message: "Multipart upload initialized" };
+    return { uploadId, key, message: 'Multipart upload initialized' };
   } catch (error) {
-    console.error("Initialize upload error:", error);
+    console.error('Initialize upload error:', error);
     res.status(500).send({
-      error: "Failed to initialize upload",
+      error: 'Failed to initialize upload',
     });
   }
 });
 
-fastify.post("/api/get-upload-part-url/", async (req, res) => {
+fastify.post('/api/get-upload-part-url/', async (req, res) => {
   try {
     const { uploadId, partNumber } = req.body;
-    console.log("get upload part url");
+    console.log('get upload part url');
     if (!multipartUploads.has(uploadId)) {
       return res.status(404).send({
-        error: "Upload ID not found",
+        error: 'Upload ID not found',
       });
     }
 
@@ -134,26 +135,26 @@ fastify.post("/api/get-upload-part-url/", async (req, res) => {
     const presignedUrl = `http://localhost:3000/api/upload-part/${uploadId}/${partNumber}`;
 
     // Simulate network delay
-    await new Promise((resolve) => setTimeout(resolve, 200));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     // console.log("upload url:", presignedUrl);
     return { url: presignedUrl };
   } catch (error) {
-    console.error("Get upload URL error:", error);
+    console.error('Get upload URL error:', error);
     res.status(500).send({
-      error: "Failed to generate upload URL",
+      error: 'Failed to generate upload URL',
     });
   }
 });
 
 // Handles actual part upload
-fastify.put("/api/upload-part/:uploadId/:partNumber", async (req, res) => {
+fastify.put('/api/upload-part/:uploadId/:partNumber', async (req, res) => {
   try {
     const { uploadId, partNumber } = req.params;
     const uploadState = multipartUploads.get(uploadId);
 
     if (!uploadState) {
       return res.status(404).send({
-        error: "Upload ID not found",
+        error: 'Upload ID not found',
       });
     }
 
@@ -164,7 +165,7 @@ fastify.put("/api/upload-part/:uploadId/:partNumber", async (req, res) => {
 
     // Generate ETag (in S3 this would be MD5 of the part)
     const partData = await fs.readFile(partFilePath);
-    const etag = crypto.createHash("md5").update(partData).digest("hex");
+    const etag = crypto.createHash('md5').update(partData).digest('hex');
 
     // Store part information
     uploadState.parts.set(parseInt(partNumber), {
@@ -174,27 +175,30 @@ fastify.put("/api/upload-part/:uploadId/:partNumber", async (req, res) => {
     });
 
     // Simulate upload delay based on file size
-    const delay = Math.min(partData.length / 10000, 2000);
-    await new Promise((resolve) => setTimeout(resolve, delay));
+    // const delay = Math.min(partData.length / 10000, 2000);
+    // random delay between 10 and 20 seconds
+    // const delay = Math.floor(Math.random() * 100);
+    // const delay = Math.floor(Math.random() * (20000 - 10000 + 1)) + 10000;
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    res.header("ETag", `"${etag}"`).status(200).send();
+    res.header('ETag', `"${etag}"`).status(200).send();
   } catch (error) {
-    console.error("Upload part error:", error);
+    console.error('Upload part error:', error);
     res.status(500).send({
-      error: "Failed to upload part",
+      error: 'Failed to upload part',
     });
   }
 });
 
 // Complete multipart upload
-fastify.post("/api/complete-multipart-upload/", async (req, res) => {
+fastify.post('/api/complete-multipart-upload/', async (req, res) => {
   try {
     const { uploadId, parts } = req.body;
     const uploadState = multipartUploads.get(uploadId);
 
     if (!uploadState) {
       return res.status(404).json({
-        error: "Upload ID not found",
+        error: 'Upload ID not found',
       });
     }
 
@@ -202,9 +206,9 @@ fastify.post("/api/complete-multipart-upload/", async (req, res) => {
     for (const { PartNumber, ETag } of parts) {
       const part = uploadState.parts.get(PartNumber);
       if (!part || part.etag !== ETag) {
-        console.log("part:", part, ETag, PartNumber);
+        console.log('part:', part, ETag, PartNumber);
         return res.status(400).send({
-          error: "Part verification failed",
+          error: 'Part verification failed',
         });
       }
     }
@@ -227,12 +231,12 @@ fastify.post("/api/complete-multipart-upload/", async (req, res) => {
 
     // Wait for file to be written
     await new Promise((resolve, reject) => {
-      writeStream.on("finish", resolve);
-      writeStream.on("error", reject);
+      writeStream.on('finish', resolve);
+      writeStream.on('error', reject);
     });
 
     // Update upload state
-    uploadState.status = "completed";
+    uploadState.status = 'completed';
     uploadState.finalPath = finalFilePath;
     uploadState.completedAt = Date.now();
 
@@ -240,28 +244,42 @@ fastify.post("/api/complete-multipart-upload/", async (req, res) => {
     await new Promise((resolve) => setTimeout(resolve, 1000));
 
     return {
-      message: "Upload completed successfully",
+      message: 'Upload completed successfully',
       location: `/uploads/${uploadState.filename}`,
     };
   } catch (error) {
-    console.error("Complete upload error:", error);
+    console.error('Complete upload error:', error);
     res.status(500).send({
-      error: "Failed to complete upload",
+      error: 'Failed to complete upload',
     });
   }
 });
 
-fastify.post("/api/complete-interview-upload/", async (req, res) => {
+fastify.post('/api/complete-interview-upload/', async (req, res) => {
   try {
     const { ...props } = req.body;
 
     return {
-      message: "Upload completed successfully",
+      message: 'Upload completed successfully',
     };
   } catch (error) {
-    console.error("Complete upload error:", error);
+    console.error('Complete upload error:', error);
     res.status(500).send({
-      error: "Failed to complete upload",
+      error: 'Failed to complete upload',
+    });
+  }
+});
+fastify.post('/api/cheating-upload/', async (req, res) => {
+  try {
+    const { ...props } = req.body;
+
+    return {
+      message: 'cheat stats uploaded',
+    };
+  } catch (error) {
+    console.error('Complete upload error:', error);
+    res.status(500).send({
+      error: 'Failed to complete upload',
     });
   }
 });
@@ -269,8 +287,8 @@ fastify.post("/api/complete-interview-upload/", async (req, res) => {
 // Step 5: Start the Server
 const startServer = async () => {
   try {
-    await fastify.listen({ port: 3000, host: "0.0.0.0" });
-    console.log("Server is running on http://localhost:3000");
+    await fastify.listen({ port: 3000, host: '0.0.0.0' });
+    console.log('Server is running on http://localhost:3000');
   } catch (err) {
     fastify.log.error(err);
     // process.env.exit(1);
